786396dae40c42b1ebd2e7052fcf2e4402ae4dad
jamie.liu <jamie.liu@ucloud.cn>
2016-01-11 16:54:00 +0800

    change ip port to fit working env

--- server/app.js
+++ server/app.js
@@ -308,6 +308,6 @@ app.post('/outputJsonFile', urlencodedParser, function (req, res) {
 })
 
 //listen (start app with node server.js)==============
-var server = app.listen(8081, function () {
+var server = app.listen(4011, function () {
 	console.log("Server Up");
 })
